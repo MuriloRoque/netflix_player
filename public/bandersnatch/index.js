d00a3fc05b5391e7ea55bd075f16b2222e90de0f
Murilo Roque <muriloengqui@gmail.com>
2020-10-28 11:02:37 -0300

    finish implementing videoPlayer component

--- public/bandersnatch/index.js
+++ public/bandersnatch/index.js
@@ -5,12 +5,15 @@ async function main() {
     const isLocal = !!~localHost.indexOf(window.location.hostname)
     const manifestJSON = await (await fetch(MANIFEST_URL)).json()
     const host = isLocal ? manifestJSON.localHost : manifestJSON.productionHost
-    const videoCOmponent = new VideoComponent()
-
+    const videoComponent = new VideoComponent()
+    const network = new Network({ host })
     const videoPlayer = new VideoMediaPlayer({
-        manifestJSON
+        manifestJSON,
+        network
     })
-    videoCOmponent.initializePlayer()
+
+    videoPlayer.initializeCodec()
+    videoComponent.initializePlayer()
 
 }
 
--- public/bandersnatch/src/videoPlayer.js
+++ public/bandersnatch/src/videoPlayer.js
@@ -1,8 +1,11 @@
 class VideoMediaPlayer {
-  constructor({ manifestJSON }) {
+  constructor({ manifestJSON, network }) {
     this.manifestJSON = manifestJSON
+    this.network = network
     this.videoElement = null
     this.sourceBuffer = null
+    this.selected = {}
+    this.videoDuration = 0
   }
 
   initializeCodec() {
@@ -12,10 +15,61 @@ class VideoMediaPlayer {
       alert('Your browser or system does not support MSE!')
       return
     }
+
     const codecSupported = MediaSource.isTypeSupported(this.manifestJSON.codec)
     if (!codecSupported) {
-      alert(`Your browser does not support codec: ${thismanifestJSON.codec}`)
+      alert(`Your browser does not support codec: ${this.manifestJSON.codec}`)
       return
     }
+
+    const mediaSource = new MediaSource()
+    this.videoElement.src = URL.createObjectURL(mediaSource)
+
+    mediaSource.addEventListener("sourceopen", this.sourceOpenWrapper(mediaSource))
+  }
+
+  sourceOpenWrapper(mediaSource) {
+    return async(_) => {
+      this.sourceBuffer = mediaSource.addSourceBuffer(this.manifestJSON.codec)
+      const selected = this.selected = this.manifestJSON.intro
+      mediaSource.duration = this.videoDuration
+      await this.fileDownload(selected.url)
+    }
+  }
+
+  async fileDownload(url) {
+    const prepareUrl = {
+      url,
+      fileResolution: 360,
+      fileResolutionTag: this.manifestJSON.fileResolutionTag,
+      hostTag: this.manifestJSON.hostTag
+    }
+    const finalUrl = this.network.parseManifestURL(prepareUrl)
+    this.setVideoPlayerDuration(finalUrl)
+    const data = await this.network.fetchFile(finalUrl)
+    return this.processBufferSegments(data)
+  }
+
+  setVideoPlayerDuration(finalURL) {
+    const bars = finalURL.split('/')
+    const [ name, videoDuration ] = bars[bars.length - 1].split('-')
+    this.videoDuration += videoDuration
+  }
+
+  async processBufferSegments(allSegments) {
+    const sourceBuffer = this.sourceBuffer
+    sourceBuffer.appendBuffer(allSegments)
+
+    return new Promise((resolve, reject) => {
+      const updateEnd = (_) => {
+        sourceBuffer.removeEventListener('updateend', updateEnd)
+        sourceBuffer.timestampOffset = this.videoDuration
+
+        return resolve()
+      }
+      
+      sourceBuffer.addEventListener('updateend', updateEnd)
+      sourceBuffer.addEventListener('error', reject)
+    })
   }
 }
